e041b216dd2045018403df93af6a27069fa73d3e
Aldrin Misquitta <aldrinm@gmail.com>
2024-04-18 12:15:41 +0100

    Default config

--- src/test/java/aldrin/displaynamegenerators/DisplayNameGeneratorCamelTests.java
+++ src/test/java/aldrin/displaynamegenerators/DisplayNameGeneratorCamelTests.java
@@ -1,7 +1,6 @@
 package aldrin.displaynamegenerators;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
